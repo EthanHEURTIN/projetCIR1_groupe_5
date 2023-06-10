f7d5d33cc6e78631000a9202128d835f84785d19
R5dM1st <emile.duplais@isen-ouest.yncrea.fr>
2023-06-10 10:03:20 +0200

    uu

--- js/function.js
+++ js/function.js
@@ -1,102 +1,199 @@
 // ------- WRITE FILE -------
 function writeFile(id_form,func) {
 
-    var element = document.createElement('a');
+  var element = document.createElement('a');
 
-    let text1 = document.getElementById(id_form);
-    let count = text1.elements.length;
-    let textToSave = func;
-    for(let i = 0;i<count-1;i++){
-        textToSave += ";" + text1[i].value;
-    }
+  let text1 = document.getElementById(id_form);
+  let count = text1.elements.length;
+  let textToSave = func;
+  for(let i = 0;i<count-1;i++){
+      textToSave += ";" + text1[i].value;
+  }
 
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(textToSave));
-    element.setAttribute('download', 'request.txt');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(textToSave));
+  element.setAttribute('download', 'request.txt');
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 
-   // text1.submit();
+ // text1.submit();
 }
 // -------------------------
+//fonction qui ajoute une phrase au dans la balise real de la page html
+function callWriteDirector() {//ecrit le realisateur dans le fichier request.txt
+  form_button = document.getElementById("director");
+  form_button.onclick = callWriteDirector;
+  writeFile("form-findByDirector", "findByDirector");
+}
+function callWriteTime() {//ecrit temp du film dans le fichier request.txt
+  form_button = document.getElementById("time");
+  form_button.onclick = callWriteTimer;
+  writeFile("form-findByTimer", "findByTimer");
+
+}
+function callWriteType() { //ecrit le genre de film dans le fichier request.txt
+  form_button = document.getElementById("type");
+  form_button.onclick = callWriteType;
+
+  writeFile("form-findByType", "findByType");
+
+}
+function callWriteTitle() {//ecrit le titre dans le fichier request.txt
+  form_button = document.getElementById("title");
+  form_button.onclick = callWriteTitle;
+  writeFile("form-findByTitle", "findByTitle");
+
+}
 
 
 // ------- READ FILE -------
 function readFileByName(fileName){
 
-    let xhr = new XMLHttpRequest();
-    do {
-        xhr.open("GET", fileName, false);
-        xhr.send(null);
+  let xhr = new XMLHttpRequest();
+  do {
+      xhr.open("GET", fileName, false);
+      xhr.send(null);
 
-    }while(xhr.status === 404);
+  }while(xhr.status === 404);
 
-    // assumes status 200
-    return xhr.responseText;
+  // assumes status 200
+  return xhr.responseText;
 }
 
 function readFile(){
-    readFileByName("ready.txt");
-    //return readFileByName("results.txt");
-    console.log(readFileByName("results.txt"));
+  readFileByName("ready.txt");
+  return readFileByName("results.txt");
+}
+// -------------------------
+
+let text = readFile();
+
+
+function tempExecution(text) {
+  let tmpData = text.split("\n")[0].split(";")[0];
+  document.getElementById("tmpData").innerHTML = "Temps d'exécution : " + tmpData + " ms";
+}
+tempExecution(text);
+function callButton() {
+//verifie quel fonction est appelé callWriteDirector, callWriteTime, callWriteType, callWriteTitle
+}
+callButton();
+
+function creerTableauFilms(text) {
+  let boubou = 'realisateur'
+  let tableau = [];
+  let lignes = text.split("\n");
+  if (boubou == "realisateur") {
+    for (let i = 1; i < lignes.length; i++) {
+      let film = {};
+      let elements = lignes[i].split(";");
+      film.titre = elements[0];
+      film.duree = elements[1];
+      film.genre = elements[2];
+      tableau.push(film);
+    
+    }
+  }
+  if (boubou == "temp") {
+    for (let i = 1; i < lignes.length; i++) {
+      let film = {};
+      let elements = lignes[i].split(";");
+      film.realisateur = elements[0];
+      film.titre = elements[1];
+      film.genre = elements[2];
+      tableau.push(film);
+    }
+  }
+  if (boubou == "genre") {
+    for (let i = 1; i < lignes.length; i++) {
+      let film = {};
+      let elements = lignes[i].split(";");
+      film.realisateur = elements[0];
+      film.titre = elements[1];
+      film.duree = elements[2];
+      tableau.push(film);
+    }
+  }
+  if (boubou == "titre") {
+    for (let i = 1; i < lignes.length; i++) {
+      let film = {};
+      let elements = lignes[i].split(";");
+      film.realisateur = elements[0];
+      film.duree = elements[1];
+      film.genre = elements[2];
+      tableau.push(film);
+    }
+  }
+  return tableau;
 }
 
-function extraireValeurs(texte) {
-    const lignes = texte.split("\n");
-  
-    const valeurs = lignes.map(function(ligne) {
-      const elements = ligne.trim().split(";");
-      const dureeReaction = parseFloat(elements[0]);
-      const titre = elements[1];
-      const duree = parseInt(elements[2]);
-      const genre = elements[3];
-  
-      return {
-        dureeReaction,
-        titre,
-        duree,
-        genre
-      };
-    });
-  
-    return valeurs;
+let tableauFilms = creerTableauFilms(text);
+console.log(tableauFilms);
+
+
+
+
+
+//creer une fonction avec un tableau et l'affiche dans le html a l'id resultat
+function afficherTableau(tableau) {
+  let html = "<table>";
+  let boubou = 'realisateur'
+
+  if (boubou == "realisateur") {
+    html += "<tr><th>Titre</th><th>Durée</th><th>Genre</th></tr>";
+
+    for (let i = 0; i < tableau.length; i++) {
+      html += "<tr>";
+      html += "<td>" + tableau[i].titre + "</td>";
+      html += "<td>" + tableau[i].duree + "</td>";
+      html += "<td>" + tableau[i].genre + "</td>";
+      html += "</tr>";
+    }
   }
 
+  if (boubou == "temp") {
+    html += "<tr><th>Realisateur</th><th>Titre</th><th>Genre</th></tr>";
 
+    for (let i = 0; i < tableau.length; i++) {
+      html += "<tr>";
+      html += "<td>" + tableau[i].realisateur + "</td>";
+      html += "<td>" + tableau[i].titre + "</td>";
+      html += "<td>" + tableau[i].genre + "</td>";
+      html += "</tr>";
+    }
+  }
 
+  if (boubou == "genre") {
+    html += "<tr><th>Realisateur</th><th>Titre</th><th>Durée</th></tr>";
 
+    for (let i = 0; i < tableau.length; i++) {
+      html += "<tr>";
+      html += "<td>" + tableau[i].realisateur + "</td>";
+      html += "<td>" + tableau[i].titre + "</td>";
+      html += "<td>" + tableau[i].duree + "</td>";
+      html += "</tr>";
+    }
+  }
 
+  if (boubou == "titre") {
+    html += "<tr><th>Realisateur</th><th>Durée</th><th>Genre</th></tr>";
 
+    for (let i = 0; i < tableau.length; i++) {
+      html += "<tr>";
+      html += "<td>" + tableau[i].realisateur + "</td>";
+      html += "<td>" + tableau[i].duree + "</td>";
+      html += "<td>" + tableau[i].genre + "</td>";
+      html += "</tr>";
+    }
+  }
 
-function callWriteDirector() {//ecrit le realisateur dans le fichier request.txt
-    writeFile("form-findByDirector", "findByDirector");
+  html += "</table>";
+  document.getElementById("resultat").innerHTML = html;
 }
-function callWriteTimer() {//ecrit temp du film dans le fichier request.txt
-    writeFile("form-findByTimer", "findByTimer");
+afficherTableau(tableauFilms);
 
-}
-function callWriteType() { //ecrit le genre de film dans le fichier request.txt
-    writeFile("form-findByType", "findByType");
-
-}
-function callWriteTitle() {//ecrit le titre dans le fichier request.txt
-    writeFile("form-findByTitle", "findByTitle");
-
-}
-//Director
-form_button = document.getElementById("director");
-form_button.onclick = callWriteDirector;
-//Time
-form_button = document.getElementById("time");
-form_button.onclick = callWriteTimer;
-//Type
-form_button = document.getElementById("type");
-form_button.onclick = callWriteType;
-//Title
-form_button = document.getElementById("title");
-form_button.onclick = callWriteTitle;
 
 
 
